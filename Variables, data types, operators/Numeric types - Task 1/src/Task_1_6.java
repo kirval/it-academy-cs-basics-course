5aef1b37689ba0988eb9050a2d7cabf68507dc93
Kirill Valyushko <valyushko.kirill@gmail.com>
2020-06-29 11:49:54 +0300

    fix lesson 1

--- Variables, data types, operators/Numeric types - Task 1/src/Task_1_6.java
+++ Variables, data types, operators/Numeric types - Task 1/src/Task_1_6.java
@@ -1,17 +1,3 @@
-import java.util.Scanner;
-
 public class Task_1_6 {
-
-
-    public static void main(String[] args) {
-        long sum = Long.parseLong(args[0]) *
-                Long.parseLong(args[1]) *
-                Long.parseLong(args[2]) *
-                Long.parseLong(args[3]) *
-                Long.parseLong(args[4]);
-
-        System.out.println("хрен = " + sum);
-
-    }
-
+    //put your task here
 }
